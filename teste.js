34248a60cd0caefefc3e5601970b1f51f37a5879
adhejnr <117763660+adhejnr@users.noreply.github.com>
2022-11-23 14:15:50 +0000

    Códigos 23/11

--- teste.js
+++ teste.js
@@ -1,4 +1,13 @@
-var x = 2;
-var y =3;
-console.log(x*y)
 var http = require('http');
+var n = require('./mdl1');
+var dia = require('./mdlDt');
+
+http.createServer(function(req, res){
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write('<b> Aula Node.js</b> <br>');
+    res.write(n.nome()+'<br>');
+    //res.writre(Date());
+    res.write(dia.diaDaSemana()+'<br>');
+    res.end("<center><h2> Olá, Boa tarde! </h2></center>");
+
+}).listen(80);
